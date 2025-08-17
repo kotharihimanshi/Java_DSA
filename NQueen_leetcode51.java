1181bf5dd03b3d5b30259f999802da5a88552d3d
Himanshi <himanshikothari9@gmail.com>
2025-08-17 15:54:12 +0530

    arrays

--- NQueen_leetcode51.java
+++ NQueen_leetcode51.java
@@ -1,8 +1,62 @@
-<<<<<<< HEAD
+
+// import java.util.Scanner;
+
+// class NQueen_leetcode51 {
+//     static int solCtr;
+
+//     public static void solveNQueen(int n) {
+//         int[] board = new int[n];
+//         for (int i = 0; i < n; i++) board[i] = -1;
+//         solCtr = 0;
+//         solveHelper(board, n, 0);
+//     }
+
+//     private static void solveHelper(int[] board, int n, int row) {
+//         if (row == n) {
+//             System.out.printf("Solution #%d:\n", ++solCtr);
+//             printBoard(board);
+//             return;
+//         }
+
+//         for (int col = 0; col < n; col++) {
+//             if (isSafe(board, row, col)) {
+//                 board[row] = col;
+//                 solveHelper(board, n, row + 1);
+//                 board[row] = -1; // backtrack
+//             }
+//         }
+//     }
+
+//     private static boolean isSafe(int[] board, int row, int col) {
+//         for (int i = 0; i < row; i++) {
+//             if (board[i] == col || Math.abs(i - row) == Math.abs(board[i] - col)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     private static void printBoard(int[] board) {
+//         for (int row = 0; row < board.length; row++) {
+//             for (int col = 0; col < board.length; col++) {
+//                 System.out.print(board[row] == col ? "Q " : "- ");
+//             }
+//             System.out.println();
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter number of queens: ");
+//         int n = sc.nextInt();
+//         solveNQueen(n);
+//     }
+
 import java.util.Scanner;
 
 class NQueen_leetcode51 {
-    static int solCtr;
+    static int solCtr;  //count of valid arrangment of queens
 
     public static void solveNQueen(int n) {
         int[] board = new int[n];
@@ -53,59 +107,4 @@ class NQueen_leetcode51 {
         solveNQueen(n);
     }
 }
-=======
-import java.util.Scanner;
 
-class NQueen_leetcode51 {
-    static int solCtr;
-
-    public static void solveNQueen(int n) {
-        int[] board = new int[n];
-        for (int i = 0; i < n; i++) board[i] = -1;
-        solCtr = 0;
-        solveHelper(board, n, 0);
-    }
-
-    private static void solveHelper(int[] board, int n, int row) {
-        if (row == n) {
-            System.out.printf("Solution #%d:\n", ++solCtr);
-            printBoard(board);
-            return;
-        }
-
-        for (int col = 0; col < n; col++) {
-            if (isSafe(board, row, col)) {
-                board[row] = col;
-                solveHelper(board, n, row + 1);
-                board[row] = -1; // backtrack
-            }
-        }
-    }
-
-    private static boolean isSafe(int[] board, int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (board[i] == col || Math.abs(i - row) == Math.abs(board[i] - col)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static void printBoard(int[] board) {
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board.length; col++) {
-                System.out.print(board[row] == col ? "Q " : "- ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of queens: ");
-        int n = sc.nextInt();
-        solveNQueen(n);
-    }
-}
->>>>>>> 7aece136fd9e9013a752b4bd1e9acd5458be2b36
--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -1,4 +1,43 @@
-<<<<<<< HEAD
+
+// public class RemoveDuplicatesFromSortedArray {
+//     public int removeDuplicates(int[] nums, int n) {
+//         if (n == 0) {
+//             return 0;
+//         }
+//         int i = 0;
+//         for (int j = 1; j < n; j++) {
+//             if (nums[i] != nums[j]) {
+//                 nums[i + 1] = nums[j];
+//                 i++;
+//             }
+//         }
+//         return i + 1;
+//     }
+
+//     // Optional main method to test
+//     public static void main(String[] args) {
+//         RemoveDuplicatesFromSortedArray solution = new RemoveDuplicatesFromSortedArray();
+
+//         int[] nums1 = {1, 1, 2};
+//         int length1 = solution.removeDuplicates(nums1, nums1.length);
+//         System.out.println("Unique count: " + length1);
+//         System.out.print("Array after removing duplicates: ");
+//         for (int i = 0; i < length1; i++) {
+//             System.out.print(nums1[i] + " ");
+//         }
+//         System.out.println();
+
+//         int[] nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+//         int length2 = solution.removeDuplicates(nums2, nums2.length);
+//         System.out.println("Unique count: " + length2);
+//         System.out.print("Array after removing duplicates: ");
+//         for (int i = 0; i < length2; i++) {
+//             System.out.print(nums2[i] + " ");
+//         }
+//         System.out.println();
+//     }
+// }
+
 public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums, int n) {
         if (n == 0) {
@@ -37,43 +76,4 @@ public class RemoveDuplicatesFromSortedArray {
         System.out.println();
     }
 }
-=======
-public class RemoveDuplicatesFromSortedArray {
-    public int removeDuplicates(int[] nums, int n) {
-        if (n == 0) {
-            return 0;
-        }
-        int i = 0;
-        for (int j = 1; j < n; j++) {
-            if (nums[i] != nums[j]) {
-                nums[i + 1] = nums[j];
-                i++;
-            }
-        }
-        return i + 1;
-    }
 
-    // Optional main method to test
-    public static void main(String[] args) {
-        RemoveDuplicatesFromSortedArray solution = new RemoveDuplicatesFromSortedArray();
-
-        int[] nums1 = {1, 1, 2};
-        int length1 = solution.removeDuplicates(nums1, nums1.length);
-        System.out.println("Unique count: " + length1);
-        System.out.print("Array after removing duplicates: ");
-        for (int i = 0; i < length1; i++) {
-            System.out.print(nums1[i] + " ");
-        }
-        System.out.println();
-
-        int[] nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        int length2 = solution.removeDuplicates(nums2, nums2.length);
-        System.out.println("Unique count: " + length2);
-        System.out.print("Array after removing duplicates: ");
-        for (int i = 0; i < length2; i++) {
-            System.out.print(nums2[i] + " ");
-        }
-        System.out.println();
-    }
-}
->>>>>>> 7aece136fd9e9013a752b4bd1e9acd5458be2b36
